d59a0d0432b783853de7f7fcf58427721ecafd6d
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2024-08-01 22:51:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,34 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Variables
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  default = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/24"
+  ]
+}
+
 # Create an S3 bucket for Kops state storage
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "kops-indojeans-state-store"
-
   tags = {
     Name = "kops-indojeans-state-store"
   }
@@ -28,7 +52,7 @@ resource "aws_dynamodb_table" "state_lock" {
 
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -36,34 +60,26 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Create subnets
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+# Create public subnets
+resource "aws_subnet" "public" {
+  count                  = length(var.public_subnet_cidrs)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = element(var.public_subnet_cidrs, count.index)
+  availability_zone      = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
   tags = {
-    Name = "kops-subnet-1"
+    Name = "kops-public-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = true
+# Create private subnets
+resource "aws_subnet" "private" {
+  count                  = length(var.private_subnet_cidrs)
+  vpc_id                 = aws_vpc.main.id
+  cidr_block             = element(var.private_subnet_cidrs, count.index)
+  availability_zone      = element(data.aws_availability_zones.available.names, count.index)
   tags = {
-    Name = "kops-subnet-2"
-  }
-}
-
-resource "aws_subnet" "subnet3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = "ap-south-1c"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "kops-subnet-3"
+    Name = "kops-private-subnet-${count.index + 1}"
   }
 }
 
@@ -75,32 +91,72 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# Create a route table
-resource "aws_route_table" "routetable" {
+# Create a NAT gateway for private subnets
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "kops-nat-gateway"
+  }
+}
+
+# Create route tables
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
-    Name = "kops-route-table"
+    Name = "kops-public-route-table"
   }
 }
 
-# Associate the route table with the subnets
-resource "aws_route_table_association" "assoc1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.routetable.id
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
+  }
+  tags = {
+    Name = "kops-private-route-table"
+  }
 }
 
-resource "aws_route_table_association" "assoc2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.routetable.id
+# Associate route tables with subnets
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "assoc3" {
-  subnet_id      = aws_subnet.subnet3.id
-  route_table_id = aws_route_table.routetable.id
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# Create VPC endpoints for private subnets
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.s3"
+  route_table_ids   = aws_route_table.private.*.id
+  tags = {
+    Name = "kops-s3-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${var.region}.dynamodb"
+  route_table_ids   = aws_route_table.private.*.id
+  tags = {
+    Name = "kops-dynamodb-endpoint"
+  }
 }
 
 # Outputs
@@ -119,16 +175,32 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "subnet_ids" {
-  description = "The IDs of the subnets."
-  value       = [
-    aws_subnet.subnet1.id,
-    aws_subnet.subnet2.id,
-    aws_subnet.subnet3.id
-  ]
+output "public_subnet_ids" {
+  description = "The IDs of the public subnets."
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets."
+  value       = aws_subnet.private[*].id
 }
 
 output "internet_gateway_id" {
   description = "The ID of the Internet Gateway."
   value       = aws_internet_gateway.gw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway."
+  value       = aws_nat_gateway.gw.id
+}
+
+output "vpc_endpoint_s3_id" {
+  description = "The ID of the VPC Endpoint for S3."
+  value       = aws_vpc_endpoint.s3.id
+}
+
+output "vpc_endpoint_dynamodb_id" {
+  description = "The ID of the VPC Endpoint for DynamoDB."
+  value       = aws_vpc_endpoint.dynamodb.id
 }
