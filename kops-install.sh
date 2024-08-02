664134a6a4588999f3c686442aacc30fdce713c0
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2024-08-02 20:34:52 +0530

    Update kops-install.sh

--- kops-install.sh
+++ kops-install.sh
@@ -4,17 +4,24 @@ export KOPS_STATE_STORE=s3://kops-indojeans-state-store
 
 # Fetch VPC ID and Subnet IDs using Terraform
 VPC_ID=$(terraform output -json vpc_id | jq -r .)
-PUBLIC_SUBNET_IDS=$(terraform output -json public_subnet_ids | jq -r '.[]' | paste -sd, -)
+#PUBLIC_SUBNET_IDS=$(terraform output -json public_subnet_ids | jq -r '.[]' | paste -sd, -)
 PRIVATE_SUBNET_IDS=$(terraform output -json private_subnet_ids | jq -r '.[]' | paste -sd, -)
+
+# Specify the path to your SSH public key
+SSH_PUBLIC_KEY_PATH=~/.ssh/my-key.pub
 
 # Create the Kops cluster
 kops create cluster \
   --name=kops.indojeans.in \
   --state=${KOPS_STATE_STORE} \
+  --master-zones=ap-south-1a,ap-south-1b,ap-south-1c \
   --zones=ap-south-1a,ap-south-1b,ap-south-1c \
   --node-count=3 \
+  --topology private \
+  --networking kopeio-vxlan \
   --node-size=t3.medium \
   --control-plane-size=t3.medium \
   --network-id=${VPC_ID} \
-  --subnets=${PUBLIC_SUBNET_IDS},${PRIVATE_SUBNET_IDS} \
+  --subnets=${PRIVATE_SUBNET_IDS} \
   --dns-zone=indojeans.in
+  --ssh-public-key=${SSH_PUBLIC_KEY_PATH}
