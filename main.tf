956c94926980108d5cecfa6bf43c7f83196e4695
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2024-08-08 16:49:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 # Create an S3 bucket for Kops state storage
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "kops-indojeans-state-store"
-
   tags = {
     Name = "kops-indojeans-state-store"
   }
@@ -20,7 +19,6 @@ resource "aws_dynamodb_table" "state_lock" {
     name = "LockID"
     type = "S"
   }
-
   tags = {
     Name = "kops-state-lock"
   }
@@ -36,34 +34,32 @@ resource "aws_vpc" "main" {
   }
 }
 
-# Create subnets
-resource "aws_subnet" "subnet1" {
+# Create public and private subnets
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
   tags = {
-    Name = "kops-subnet-1"
+    Name = "kops-public-subnet-1"
   }
 }
 
-resource "aws_subnet" "subnet2" {
+resource "aws_subnet" "private_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = true
   tags = {
-    Name = "kops-subnet-2"
+    Name = "kops-private-subnet-1"
   }
 }
 
-resource "aws_subnet" "subnet3" {
+resource "aws_subnet" "private_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "ap-south-1c"
-  map_public_ip_on_launch = true
   tags = {
-    Name = "kops-subnet-3"
+    Name = "kops-private-subnet-2"
   }
 }
 
@@ -75,32 +71,56 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# Create a route table
-resource "aws_route_table" "routetable" {
+# Create a NAT Gateway in the public subnet
+resource "aws_eip" "nat_eip" {
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name = "kops-nat-gateway"
+  }
+}
+
+# Create route table for public subnets
+resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
-    Name = "kops-route-table"
+    Name = "kops-public-route-table"
   }
 }
 
-# Associate the route table with the subnets
-resource "aws_route_table_association" "assoc1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.routetable.id
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
-resource "aws_route_table_association" "assoc2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.routetable.id
+# Create route table for private subnets
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = {
+    Name = "kops-private-route-table"
+  }
 }
 
-resource "aws_route_table_association" "assoc3" {
-  subnet_id      = aws_subnet.subnet3.id
-  route_table_id = aws_route_table.routetable.id
+resource "aws_route_table_association" "private_association_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_association_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 # Outputs
@@ -122,13 +142,18 @@ output "vpc_id" {
 output "subnet_ids" {
   description = "The IDs of the subnets."
   value       = [
-    aws_subnet.subnet1.id,
-    aws_subnet.subnet2.id,
-    aws_subnet.subnet3.id
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
   ]
 }
 
 output "internet_gateway_id" {
   description = "The ID of the Internet Gateway."
   value       = aws_internet_gateway.gw.id
+}
+
+output "nat_gateway_id" {
+  description = "The ID of the NAT Gateway."
+  value       = aws_nat_gateway.nat_gw.id
 }
